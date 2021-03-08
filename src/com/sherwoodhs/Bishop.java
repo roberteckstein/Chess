6c226b22de1367299ee7fed0f0baac02c74e7c2e
haydenbd22 <haydenbd22@sherwoodsd.com>
2021-03-08 14:33:32 -0800

    Added Bishop class that extends Piece, will work on more later

---
+++ src/com/sherwoodhs/Bishop.java
@@ -0,0 +1,12 @@
+package com.sherwoodhs;
+
+public class Bishop extends Piece{
+
+    public Bishop(boolean white) {
+        super(white);
+    }
+
+    public boolean canMove(Board board, Spot start, Spot end) {
+        return false;
+    }
+}
