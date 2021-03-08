65287eeebae0520e24a850ae68b6effc0f1f1bc2
Sankalp P <sankalpsp21@sherwoodsd.com>
2021-03-08 22:35:48 +0000

    Asked user for starting and ending square in Main

--- src/com/sherwoodhs/Main.java
+++ src/com/sherwoodhs/Main.java
@@ -1,4 +1,5 @@
 package com.sherwoodhs;
+import java.util.Scanner;
 
 public class Main {
 
@@ -12,9 +13,28 @@ public class Main {
 
         int startX1, startY1, startX2, startY2, endX1, endY1, endX2, endY2;
 
+        Scanner input = new Scanner(System.in);
+
         while (true) {
 
             //  Ask player 1 for a move
+            System.out.println("Enter starting square's X position");
+
+             startX1 = input.nextInt();
+
+            System.out.println("Enter starting square's Y position");
+
+             startY1 = input.nextInt();
+
+            System.out.println("Enter ending square's X position");
+
+             endX1 = input.nextInt();
+
+            System.out.println("Enter ending square's Y position");
+
+             endY1 = input.nextInt();
+
+           
 
           //  game.playerMove(player1, startX1, startY1, endX1, endY1);
             if (game.isEnd()) {
@@ -22,6 +42,22 @@ public class Main {
             }
 
             //  Ask player 2 for a move
+
+            System.out.println("Enter starting square's X position");
+
+             startX2 = input.nextInt();
+
+            System.out.println("Enter starting square's Y position");
+
+             startY2 = input.nextInt();
+
+            System.out.println("Enter ending square's X position");
+
+             endX2 = input.nextInt();
+
+            System.out.println("Enter ending square's Y position");
+
+             endY2 = input.nextInt();
 
          //   game.playerMove(player2, startX2, startY2, endX2, endY2);
             if (game.isEnd()) {
