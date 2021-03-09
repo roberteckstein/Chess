0c746132daadcc15ad63a3681e684cf94e885bfc
haydenbd22 <haydenbd22@sherwoodsd.com>
2021-03-09 13:52:29 -0800

    Added some code for Bishop's canMove

--- src/com/sherwoodhs/Bishop.java
+++ src/com/sherwoodhs/Bishop.java
@@ -7,6 +7,13 @@ public class Bishop extends Piece{
     }
 
     public boolean canMove(Board board, Spot start, Spot end) {
-        return false;
+        if(end.getPiece().isWhite() == this.isWhite()) {
+            return false;
+        }
+
+        int x = Math.abs(start.getX() - end.getX());
+        int y = Math.abs(start.getY() - end.getY());
+
+        return (x == y && (x != 0 || y != 0));
     }
 }
