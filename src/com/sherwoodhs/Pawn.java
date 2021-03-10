90543f43b616d52e1154c2d257e9af010bb2a6f7
AnthonyRobin <anthonyrobincr22@sherwoodsd.com>
2021-03-10 03:21:47 +0000

    Made slight tweeks to Pawn canMove(not finished)

--- src/com/sherwoodhs/Pawn.java
+++ src/com/sherwoodhs/Pawn.java
@@ -17,37 +17,38 @@ public class Pawn extends Piece {
   public void promotion() {
     
   }
+/*
+@Override
+public boolean canMove(Board board,Spot start, Spot end) {
 
-//  @Override
-//  public boolean canMove(Board board,Spot start, Spot end) {
-//
-//    //Stops pawn from moving onto space with another white piece
-//    if (end.getPiece().isWhite() == this.isWhite()) {
-//      return false;
-//    }
-//
-//    int x = Math.abs(start.getX() - end.getX());
-//    int y = Math.abs(start.getY() - end.getY());
-//
-//    //If pawn moves forward 1 space
-//    if (y == 1) {
-//      return true;
-//
-//    //If the pawn is capturing another piece moving 1 diagonally is legal
-//    } else if (x + y == 2 || x + y == 0) {
-//      if(!end.getPiece.isWhite()) {
-//        return true;
-//      }else{
-//        return false;
-//      }
-//
-//    //If it's the pawns first move, moving two spaces is legal
-//    } else if (firstMove && y == 2) {
-//      firstMove = false;
-//      return true;
-//    }
-//  }
+  //Stops pawn from moving onto space with another white piece
+  if (end.getPiece().isWhite() == this.isWhite()) {
+    return false;
+  }
 
+    int x = Math.abs(start.getX() - end.getX());
+    int y = Math.abs(start.getY() - end.getY());
+
+    //If pawn moves forward 1 space
+    if (y == 1 && x == 0) {
+     return true;
+
+   //If the pawn is capturing another piece moving 1 diagonally is legal
+    } else if (x + y == 2 || x + y == 0) {
+      if(end.isEmpty()) {
+        return false;
+      }else{
+        return true;
+      }
+
+    //If it's the pawns first move, moving two spaces is legal
+    } else if (firstMove && y == 2 && x == 0) {
+        //Currently has ability to pass through pieces, need to fix
+        firstMove = false;
+        return true;
+    }
+  }
+*/
   public String toString() {
     if (this.isWhite()) {
       return " wP ";
