5473eb04d6b63a109ee94206f4bf6aa70b7dc5c3
rmkirk21 <rmkirkpatrick21@gmail.com>
2021-03-09 21:29:27 -0800

    print board (rank and files added)

--- src/com/sherwoodhs/Board.java
+++ src/com/sherwoodhs/Board.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class Board {
     Spot[][] boxes = new Spot[8][8];
+    final char[] files = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+    boolean white = true;
 
     public Board() {
         this.resetBoard();
@@ -61,6 +63,7 @@ public class Board {
 
     public void flipBoard() {
         Spot[][] temp = new Spot[8][8];
+        white = !white;
 
         // make new identical array
         for (int i = 0; i < 8; i++) {
@@ -72,7 +75,7 @@ public class Board {
         // flip array
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                boxes[i][j] = temp[Math.abs(i-7)][Math.abs(j-7)];
+                boxes[i][j] = temp[Math.abs(i - 7)][Math.abs(j - 7)];
             }
         }
 
@@ -82,7 +85,28 @@ public class Board {
     public String toString() {
         StringBuilder drawBoard = new StringBuilder();
 
+        // add letters for files
+        if (white) {
+            for (int i = 0; i < 8; i++) {
+                drawBoard.append("  ").append(files[i]).append("  ");
+            }
+            drawBoard.append("\n");
+        } else {
+            for (int i = 0; i < 8; i++) {
+                drawBoard.append("  ").append(files[Math.abs(i - 7)]).append("  ");
+            }
+            drawBoard.append("\n");
+        }
+
+        // add board layout and rank numbers
         for (int i = 0; i < 8; i++) {
+            if (white) {
+                drawBoard.append(i + 1);
+            } else {
+                drawBoard.append(Math.abs(i - 8));
+            }
+
+
             for (int j = 0; j < 8; j++) {
                 drawBoard.append(boxes[i][j]).append(" ");
             }
