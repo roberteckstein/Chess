f41fdb293714edd01a8035c310901e6d59615922
rmkirk21 <rmkirkpatrick21@gmail.com>
2021-03-10 00:15:09 -0800

    small changes to fix pawns

--- src/com/sherwoodhs/Board.java
+++ src/com/sherwoodhs/Board.java
@@ -25,8 +25,8 @@ public class Board {
         boxes[0][0] = new Spot(0, 0, new Rook(true));
         boxes[0][1] = new Spot(0, 1, new Knight(true));
         boxes[0][2] = new Spot(0, 2, new Bishop(true));
-        boxes[0][3] = new Spot(0, 3, new King(true));
-        boxes[0][4] = new Spot(0, 4, new Queen(true));
+        boxes[0][3] = new Spot(0, 3, new Queen(true));
+        boxes[0][4] = new Spot(0, 4, new King(true));
         boxes[0][5] = new Spot(0, 5, new Bishop(true));
         boxes[0][6] = new Spot(0, 6, new Knight(true));
         boxes[0][7] = new Spot(0, 7, new Rook(true));
@@ -41,20 +41,20 @@ public class Board {
         boxes[7][0] = new Spot(7, 0, new Rook(false));
         boxes[7][1] = new Spot(7, 1, new Knight(false));
         boxes[7][2] = new Spot(7, 2, new Bishop(false));
-        boxes[7][3] = new Spot(7, 3, new King(false));
-        boxes[7][4] = new Spot(7, 4, new Queen(false));
+        boxes[7][3] = new Spot(7, 3, new Queen(false));
+        boxes[7][4] = new Spot(7, 4, new King(false));
         boxes[7][5] = new Spot(7, 5, new Bishop(false));
         boxes[7][6] = new Spot(7, 6, new Knight(false));
         boxes[7][7] = new Spot(7, 7, new Rook(false));
 
-        // initialize white pawn row
+        // initialize black pawn row
 
         for (int i = 0; i < 8; i++) {
             boxes[6][i] = new Spot(6, i, new Pawn(false));
         }
 
         // initialize other squares
-        for (int i = 2; i <= 6; i++) {
+        for (int i = 2; i <= 5; i++) {
             for (int j = 0; j < 8; j++) {
                 boxes[i][j] = new Spot(i, j, null);
             }
