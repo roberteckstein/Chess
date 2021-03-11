a272f2c1413083f0276b55b476027b54c6e337de
haydenbd22 <haydenbd22@sherwoodsd.com>
2021-03-11 13:48:25 -0800

    Added checks for which directions Bishop is moving

--- src/com/sherwoodhs/Bishop.java
+++ src/com/sherwoodhs/Bishop.java
@@ -17,12 +17,21 @@ public class Bishop extends Piece{
       if(Math.abs(end.getX() - start.getX()) != Math.abs(end.getY() - start.getY())) {
           // Checking if not moved diagonally
            return false;
-
       } else {
           if(Math.abs(start.getY() - end.getX()) != Math.abs(start.getY() - end.getY())) {
                 // Checking if movement was diagonal
                 return false;
-            }
+            } else {
+              if(start.getX() < end.getX() && start.getY() < end.getY()) {
+                  // Moving right/up
+              } else if(start.getX() > end.getX() && start.getY() < end.getY()) {
+                  // Moving left/up
+              } else if(start.getX() < end.getX() && start.getY() > end.getY()) {
+                  // Moving right/down
+              } else if(start.getX() > end.getX() && start.getY() > end.getY()) {
+                  // Moving left/down
+              }
+          }
         }
         
       // generic return statement to avoid error while testing
