c93b097cecc8d90bd4216584d31c51548243c142
Sankalp P <sankalpsp21@sherwoodsd.com>
2021-03-11 05:19:45 +0000

    Syntax errors

--- src/com/sherwoodhs/Bishop.java
+++ src/com/sherwoodhs/Bishop.java
@@ -13,19 +13,23 @@ public class Bishop extends Piece{
       if (end.getPiece().isWhite() == this.isWhite()) {
         return false;
         
-      } else {
+      }
 
-        if(end.getX() - start.getX() != end.getY() - start.getY()) {
-            // Checking if not moved diagonally
-            return false;
-            
-        } else {
-            if(Math.abs(start.getY() - end.getX()) != Math.abs(start.getY() - end.getY())) {
+      if(Math.abs(end.getX() - start.getX()) != Math.abs(end.getY() - start.getY())) {
+          // Checking if not moved diagonally
+           return false;
+
+      } else {
+          if(Math.abs(start.getY() - end.getX()) != Math.abs(start.getY() - end.getY())) {
                 // Checking if movement was diagonal
                 return false;
             }
         }
+        
+      // generic return statment to avoid error while testing
+      return false;
     }
+
     }
 
     public String toString() {
--- src/com/sherwoodhs/Knight.java
+++ src/com/sherwoodhs/Knight.java
@@ -7,19 +7,16 @@ public class Knight extends Piece {
     }
 
     @Override
-    public boolean canMove(Board board, Spot start,
-                           Spot end)
-    {
-        // we can't move the piece to a spot that has
-        // a piece of the same colour
+    public boolean canMove(Board board, Spot start, Spot end){
 
+        // we can't move the piece to a spot that has
+        // a piece of the same color
         if (end.getPiece().isWhite() == this.isWhite()) {
             return false;
         }
 
         //  Knight must move two spaces in one direction, and
         //  one space in the other.
-
         int x = Math.abs(start.getX() - end.getX());
         int y = Math.abs(start.getY() - end.getY());
         return x * y == 2;
