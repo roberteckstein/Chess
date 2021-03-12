d6aecc68ca825b4f06b79e63f96ed54bce11972f
Sankalp P <sankalpsp21@sherwoodsd.com>
2021-03-12 21:55:47 +0000

    Added rook logic to queen

--- src/com/sherwoodhs/Queen.java
+++ src/com/sherwoodhs/Queen.java
@@ -1,76 +1,118 @@
 package com.sherwoodhs;
 
-public class Queen extends Piece{
+public class Queen extends Piece {
 
-    // Andrew Hiser and Hayden deBoer
-    // Andrew have you been working on this at all?
+  // Andrew Hiser and Hayden deBoer
+  // Andrew have you been working on this at all?
 
-    public Queen(boolean white) {
-        super(white);
+  public Queen(boolean white) {
+    super(white);
+  }
+
+  public boolean canMove(Board board, Spot start, Spot end) {
+
+    int deltaX = end.getX() - start.getX();
+    int deltaY = end.getY() - start.getY();
+
+    if (end.getPiece().isWhite() == this.isWhite()) {
+      return false;
     }
 
-    public boolean canMove(Board board, Spot start, Spot end) {
-
-        int deltaX = end.getX() - start.getX();
-        int deltaY = end.getY() - start.getY();
-
-        if(end.getPiece().isWhite() == this.isWhite()) {
+    // Checking if moved diagonally
+    if (Math.abs(deltaX) == Math.abs(deltaY)) {
+      // Moving up right
+      if (deltaX > 0 && deltaY > 0) {
+        for (int i = 0; i < Math.abs(deltaX); i++) {
+          if (!board.getBox(start.getX() + i, start.getY() + i).isEmpty()) {
             return false;
+          }
+          return true;
+        }
+      }
+      // Moving up left
+      else if (deltaX < 0 && deltaY > 0) {
+        for (int i = 0; i < Math.abs(deltaX); i++) {
+          if (!board.getBox(start.getX() - i, start.getY() + i).isEmpty()) {
+            return false;
+          }
+          return true;
+        }
+      }
+      // Moving down right
+      else if (deltaX > 0 && deltaY < 0) {
+        for (int i = 0; i < Math.abs(deltaX); i++) {
+          if (!board.getBox(start.getX() + i, start.getY() - i).isEmpty()) {
+            return false;
+          }
+          return true;
+        }
+      }
+      // Moving down left
+      else if (deltaX < 0 && deltaY < 0) {
+        for (int i = 0; i < Math.abs(deltaX); i++) {
+          if (!board.getBox(start.getX() - i, start.getY() - i).isEmpty()) {
+            return false;
+          }
+          return true;
+        }
+      }
+    }
+    // Checking if moved along only one axis
+    else if ((deltaX == 0 && deltaY != 0) || (deltaX != 0 && deltaY == 0)) {
+
+      // moving right
+      if (deltaX > 0) {
+        for (int i = start.getX() + 1; i < end.getX(); i++) {
+          if (!board.getBox(i, start.getY()).isEmpty()) {
+            return false;
+          }
         }
 
-        // Checking if moved diagonally
-        if(Math.abs(deltaX) == Math.abs(deltaY)) {
-            //Moving up right
-            if(deltaX > 0 && deltaY > 0){
-                for(int i = 0; i < Math.abs(deltaX); i++){
-                    if(!board.getBox(start.getX() + i,  start.getY() + i).isEmpty()) {
-                        return false;
-                    }
-                    return true;
-                }
-            }
-            //Moving up left
-            else if(deltaX < 0 && deltaY > 0){
-                for(int i = 0; i < Math.abs(deltaX); i++) {
-                    if(!board.getBox(start.getX() - i, start.getY() + i).isEmpty()) {
-                        return false;
-                    }
-                    return true;
-                }
-            }
-            //Moving down right
-            else if(deltaX > 0 && deltaY < 0){
-                for(int i = 0; i < Math.abs(deltaX); i++) {
-                    if(!board.getBox(start.getX() + i, start.getY() - i).isEmpty()) {
-                        return false;
-                    }
-                    return true;
-                }
-            }
-            //Moving down left
-            else if(deltaX < 0 && deltaY < 0){
-                for(int i = 0; i < Math.abs(deltaX); i++) {
-                    if(!board.getBox(start.getX() - i, start.getY() - i).isEmpty()) {
-                        return false;
-                    }
-                    return true;
-                }
-            }
+        return true;
+      }
+      // moving left
+      else if (deltaX < 0) {
+        for (int i = start.getX() - 1; i > end.getX(); i--) {
+          if (!board.getBox(i, start.getY()).isEmpty()) {
+            return false;
+          }
         }
-        // Checking if moved along only one axis
-        //else if(deltaX == 0 ^ deltaY == 0) {
 
-        //}
+        return true;
+      }
+      // moving up
+      else if (deltaY > 0) {
+        for (int i = start.getY() + 1; i < end.getY(); i++) {
+          if (!board.getBox(start.getX(), i).isEmpty()) {
+            return false;
+          }
+        }
 
+        return true;
+      }
+      // moving down
+      else if (deltaY < 0) {
+        for (int i = start.getY() - 1; i > end.getY(); i--) {
+          if (!board.getBox(start.getX(), i).isEmpty()) {
+            return false;
+          }
+        }
+        return true;
+      } else {
         return false;
+      }
+
     }
 
-    public String toString() {
-        if (this.isWhite()) {
-            return " wQ ";
-        } else {
-            return " bQ ";
-        }
+    return false;
+  }
+
+  public String toString() {
+    if (this.isWhite()) {
+      return " wQ ";
+    } else {
+      return " bQ ";
     }
+  }
 
 }
