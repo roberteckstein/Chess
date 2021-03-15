9d2c3256ddfa90c2b45aefdadd7cbf09a1fa3c87
AnthonyRobin <anthonyrobincr22@sherwoodsd.com>
2021-03-15 21:06:42 +0000

    Finished Pawn canMove and uploaded what I currently have for promotion

--- src/com/sherwoodhs/Pawn.java
+++ src/com/sherwoodhs/Pawn.java
@@ -8,29 +8,26 @@ public class Pawn extends Piece {
     super(white);
   }
 
-
-  // temp for testing
-  @Override
-  public boolean canMove(Board board, Spot start, Spot end) {
-    return false;
-  }
-
-  @Override
-  public boolean performMove(Board board, Spot start, Spot end) {
-    //  Will need to do something here to perform promotion
+  //For testing
+  @Override boolean performMove(Board board, Spot start, Spot end) {
     return true;
   }
+  
+  /* Unfinished
+  @Override
+  public boolean performMove(Board board, Spot start, Spot end) {
+    // Will need to do something here to perform promotion
+    int x = end.getX();
+    int y = end.getY();
 
-  /*
-    public void pawnPromotion(Board board, int x, int y) {
-      end.getPiece().setKilled();
-        if (this.isWhite()) {
-          boxes[x][y] = new Spot(x, y, new Queen(true));
-        } else {
-          boxes[x][y] = new Spot(x, y, new Queen(false));
-      }
-    }
+    
 
+    this.getPiece().setKilled();
+    move.getEnd().setPiece(move.getStart().getPiece());
+    move.getStart().setPiece(null);
+    return true;
+  }
+*/
     @Override
     public boolean canMove(Board board,Spot start, Spot end) {
 
@@ -39,41 +36,44 @@ public class Pawn extends Piece {
         return false;
       }
 
-        int x = Math.abs(start.getX() - end.getX());
-        int y = Math.abs(start.getY() - end.getY());
+      int x = Math.abs(start.getX() - end.getX());
+      int y = Math.abs(start.getY() - end.getY());
 
 
-        //If pawn moves forward 1 space
-        if (y == 1 && x == 0 && end.isEmpty()) {
+      //If pawn moves forward 1 space
+      if (y == 1 && x == 0 && end.isEmpty()) {
+        firstMove = false;
+        return true;
+
+      //If the pawn is capturing another piece moving 1 diagonally it is legal
+      } else if (x + y == 2 || x + y == 0) {
+        if(end.isEmpty()) {
+          return false;
+        }else{
           firstMove = false;
           return true;
-
-          //If the pawn is capturing another piece moving 1 diagonally it is legal
-         } else if (x + y == 2 || x + y == 0) {
-          if(end.isEmpty()) {
-            return false;
-          }else{
-            firstMove = false;
-            return true;
-          }
-
-        //If it's the pawns first move, moving two spaces is legal
-        } else if (firstMove && y == 2 && x == 0) {
-            if (end.getY() - 1 == 1 && !end.isEmpty()) {
-                return false;
-            }
-            firstMove = false;
-            return true;
         }
+
+      //If it's the pawns first move, moving two spaces is legal
+      } else if (firstMove && y == 2 && x == 0) {
+        if (end.getY() - 1 == 1 && !end.isEmpty()) {
+          return false;
+        } else {
+          firstMove = false;
+          return true;
+        }
+      } else {
+        return false;
       }
-  */
-  public String toString() {
-    if (this.isWhite()) {
-      return " wP ";
-    } else {
-      return " bP ";
     }
-  }
+
+    public String toString() {
+      if (this.isWhite()) {
+        return " wP ";
+      } else {
+        return " bP ";
+      }
+    }
 
 }
 
